9a87894cc8d1b74042be97d8ffd34ba8eae962c8
Maria Rangelova <maria.rangelova93@gmail.com>
2016-02-09 22:18:40 +0200

    Update draw and update methods
      *Score and Level are now shown on screen during game play
      *Game speed now depends on level
      *When game is lost highscore is submitted

--- lib/GameWindow.rb
+++ lib/GameWindow.rb
@@ -1,9 +1,24 @@
 class GameWindow < Gosu::Window
+  LEVEL_UPDATE_INTERVAL = {
+    0 => 60,
+    1 => 50,
+    2 => 40,
+    3 => 35,
+    4 => 30,
+    5 => 25,
+    6 => 20,
+    7 => 15,
+    8 => 10,
+    9 => 5,
+  }
+
   def initialize
     super 550, 480
     self.caption = 'Tetris'
     @game = Game.new
     @grid = Grid.new(@game)
+    @font = Gosu::Font.new(self, "Arial", 27)
+    @last_time = 0
   end
 
   def needs_cursor?
@@ -22,11 +37,26 @@ class GameWindow < Gosu::Window
   end
 
   def draw
-    @grid.draw
+    if @game.over?
+      @font.draw('GAME OVER', 150, 50, 0, scale_x = 1, scale_y = 1, color = 0xff_ffffff)
+      @font.draw('SCORE', 150, 90, 0, scale_x = 1, scale_y = 1, color = 0xff_ffffff)
+      @font.draw("#{@game.scoring_system.score}", 150, 110, 0, scale_x = 1, scale_y = 1, color = 0xff_ffffff)
+    else
+      @font.draw('SCORE', 330, 100, 0, scale_x = 1, scale_y = 1, color = 0xff_ffffff)
+      @font.draw("#{@game.scoring_system.score}", 330, 120, 0, scale_x = 1, scale_y = 1, color = 0xff_ffffff)
+      @font.draw('LEVEL', 330, 150, 0, scale_x = 1, scale_y = 1, color = 0xff_ffffff)
+      @font.draw("#{@game.scoring_system.level}", 330, 170, 0, scale_x = 1, scale_y = 1, color = 0xff_ffffff)
+      @grid.draw
+    end
   end
 
   def update
-
+    if @game.over?
+      @game.scoring_system.submit_highscore
+    else
+      @game.update if @last_time % LEVEL_UPDATE_INTERVAL[@game.scoring_system.level] == 0
+      @last_time += 1
+    end
   end
 
   class Grid
