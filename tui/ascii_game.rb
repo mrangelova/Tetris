716f9bf0b4dfc851e948cd4903b4afeb78817f44
Maria Rangelova <maria.rangelova93@gmail.com>
2016-02-12 17:24:48 +0200

    Make render_game method private

--- tui/ascii_game.rb
+++ tui/ascii_game.rb
@@ -11,20 +11,6 @@ module Tetris
         system 'cls'
       end
 
-      def render_game
-        system 'cls'
-        puts @ascii_grid.render
-        puts "\n LEVEL: #{@game.scoring_system.level}"
-        puts "\n SCORE: #{@game.scoring_system.score}"
-        puts "\n CONTROLLERS:\n"
-        puts ' J - MOVE LEFT'
-        puts ' L - MOVE RIGHT'
-        puts ' K - DROP'
-        puts ' I - ROTATE'
-        puts ' M - DROP TO BOTTOM'
-        puts ' Q - QUIT'
-      end
-
       def run
         loop do
           execute_user_command
@@ -55,6 +41,20 @@ module Tetris
           when 'm', 'M' then @game.tetromino.drop_to_bottom
           when 'q', 'Q' then @user_playing = false
         end
+      end
+
+      def render_game
+        system 'cls'
+        puts @ascii_grid.render
+        puts "\n LEVEL: #{@game.scoring_system.level}"
+        puts "\n SCORE: #{@game.scoring_system.score}"
+        puts "\n CONTROLLERS:\n"
+        puts ' J - MOVE LEFT'
+        puts ' L - MOVE RIGHT'
+        puts ' K - DROP'
+        puts ' I - ROTATE'
+        puts ' M - DROP TO BOTTOM'
+        puts ' Q - QUIT'
       end
     end
   end
